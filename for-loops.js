69b8305849a2b2a48cc6e32529aa18d5501d12df
Russell Ries <riesru@gmail.com>
2016-04-12 12:36:17 -0500

    finish exercise

--- for-loops.js
+++ for-loops.js
@@ -12,3 +12,35 @@ console.log("Current value is ", i);
 for (var i = 4096; i >= 1; i = i / 2) {
   console.log("Current value is ", i);
 }
+
+//create an array that contains the names of American Presidents.
+//loop over that array with a for loop and use string concatenation
+//with console.log to output the order and name of each Pres
+
+var presidents = ["George Washington", "John Adams", "Thomas Jefferson", "John Quincey Adams"];
+
+for (var i = 0; i < presidents.length; i++) {
+  console.log("President #" + (i + 1 ) + " is " + presidents[i]);
+}
+
+
+//iterate over the objects below and use a for loop to output the names of the keys
+//in the objects
+
+var antSpecies = {
+  "argentine": {},
+  "army": {},
+  "big headed": {},
+  "black": {},
+  "bull": {},
+  "carpenter": {},
+  "crazy": {},
+  "fire": {},
+  "glider": {},
+  "honey pot": {},
+  "jack jumper": {}
+};
+
+for (var key in antSpecies) {
+  console.log("ant = " + key);
+}

8307834fe53f0301b84e3997773213b7d74a6421
Russell Ries <riesru@gmail.com>
2016-04-12 10:49:16 -0500

    complete first two problems

--- for-loops.js
+++ for-loops.js
@@ -1,5 +1,14 @@
-var counting = []
+//for loop initialized at 5 with increments of 10 that doesn't go
+//over 120, use console.log to output the value every time
+
 for (var i = 5; i < 120; i = i + 10) {
-  counting.push(i);
+console.log("Current value is ", i);
 }
-console.log(counting);
+
+
+//for loop initialized at 4096 that divides by 2 and stops at 1
+//use console.log to output the value every time
+
+for (var i = 4096; i >= 1; i = i / 2) {
+  console.log("Current value is ", i);
+}
